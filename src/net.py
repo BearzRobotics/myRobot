c27c2eb1785d9532f9785173ef7d63b7d168afc2
Dakota James Owen Keeler <Bearzrobotics@gmail.com>
2017-11-01 10:14:22 -0400

    add support for reading from the netconsle

--- src/net.py
+++ src/net.py
@@ -22,44 +22,8 @@ import socket
 from struct import * # interpret bytes as packed binary data
 import time
 Server = '192.168.1.68' # Hard coded for now
-# Raw bytes for the FRC_2015 protocol
-Test               = 0x01;
-Enabled            = 0x04;
-Autonomous         = 0x02;
-Teleoperated       = 0x00;
-FMS_Attached       = 0x08;
-EmergencyStop      = 0x80;
-RequestReboot      = 0x08;
-RequestNormal      = 0x80;
-RequestUnconnected = 0x00;
-RequestRestartCode = 0x04;
-FMS_RadioPing      = 0x10;
-FMS_RobotPing      = 0x08;
-FMS_RobotComms     = 0x20;
-FMS_DS_Version     = 0x00;
-TagDate            = 0x0f;
-TagGeneral         = 0x01;
-TagJoystick        = 0x0c;
-TagTimezone        = 0x10;
-Red1               = 0x00;
-Red2               = 0x01;
-Red3               = 0x02;
-Blue1              = 0x03;
-Blue2              = 0x04;
-Blue3              = 0x05;
-RTagCANInfo        = 0x0e;
-RTagCPUInfo        = 0x05;
-RTagRAMInfo        = 0x06;
-RTagDiskInfo       = 0x04;
-RequestTime        = 0x01;
-RobotHasCode       = 0x20;
 
-def writeNc(Message):
-	Wn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	Port = 6666
-	Wn.connect((Server, Port))
-	Wn.sendto(str(Message).encode('utf-8'), (Server, Port))
-	Wn.close()
+
 
 def writeStatus(Message):
 	Ws = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -68,6 +32,25 @@ def writeStatus(Message):
 	Ws.connect((StatusServer, Port))
 	Ws.sendto(str(Message).encode('utf-8'), (StatusServer, Port))
 	Ws.close()
+
+
+class netConsole:
+	
+	def readNc(self):
+		R = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		port = 6666
+		R.bind(('', port))
+		R.settimeout(10000000)
+		data, addr = R.recvfrom(4096)
+		decoded = data.decode('utf-8')
+		print (decoded)
+			
+	def writeNc(self, Message):
+		Wn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		Port = 6666
+		Wn.connect((Server, Port))
+		Wn.sendto(str(Message).encode('utf-8'), (Server, Port))
+		Wn.close()
 
 class ds:
 	def __init__(self):
@@ -92,35 +75,34 @@ class ds:
 	#Ax stands for joystick Axis
 	#	-- Returns a flout of the joystick or other axis
 	#This protocol jupports 16 buttons and 5 axis at the most. The defualt value is null resulting in a null byte being sent
-	def writeDs(ControlByte, Direction=None, Bt1=None, Bt2=None, Bt3=None, Bt4=None, Bt5=None, Bt6=None, Bt7=None, Bt8=None, Bt9=None, Bt10=None, Bt11=None, Bt12=None, Bt13=None, Bt14=None, Bt15=None, B1t6=None, Ax1=None, Ax3=None, Ax3=None, Ax4=None, Ax5=None ):
+	def writeDs(ControlByte, Direction=None, Bt1=None, Bt2=None, Bt3=None, Bt4=None, Bt5=None, Bt6=None, Bt7=None, Bt8=None, Bt9=None, Bt10=None, Bt11=None, Bt12=None, Bt13=None, Bt14=None, Bt15=None, B1t6=None, Ax1=None, Ax2=None, Ax3=None, Ax4=None, Ax5=None ):
 		W = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		Port = 7150
 		W.connect((Server, Port))
 		
 		# For format refer back to https://docs.python.org/3.0/library/struct.html
 		# For debug we'll print our new data
-		print(ControlByte, Direction, Bt1, Bt2, Bt3, Bt4, Bt5, Bt6, Bt7, Bt8, Bt9, Bt10, Bt11, Bt12, Bt13, Bt14, Bt15, B1t6, Ax1, Ax3, Ax3, Ax4, Ax5)
+		print(ControlByte, Direction, Bt1, Bt2, Bt3, Bt4, Bt5, Bt6, Bt7, Bt8, Bt9, Bt10, Bt11, Bt12, Bt13, Bt14, Bt15, B1t6, Ax1, Ax3, Ax2, Ax4, Ax5)
 		
 		# Format-	C Type	-	Python type	-	Standard Size
 		# c			char	    str len of 1			1
 		# ?			bool		bool					1
 		# f			float		float					4
 		
-		data = pack('cc????????????????fffff', ControlByte, Direction, Bt1, Bt2, Bt3, Bt4, Bt5, Bt6, Bt7, Bt8, Bt9, Bt10, Bt11, Bt12, Bt13, Bt14, Bt15, B1t6, Ax1, Ax3, Ax3, Ax4, Ax5)
+		data = pack('cc????????????????fffff', ControlByte, Direction, Bt1, Bt2, Bt3, Bt4, Bt5, Bt6, Bt7, Bt8, Bt9, Bt10, Bt11, Bt12, Bt13, Bt14, Bt15, B1t6, Ax1, Ax2, Ax3, Ax4, Ax5)
 		W.sendto(data, (Server, Port))
 		W.close()
-
 
 def mode():
 	mode = ds.readDs()
 	#print(mode)
 	return mode	
 	
-class frc_2015:
+class decrypt:
 	def decryptPacket():
 		DSP = ds.readDs()
 		print(DSP)
-		DECP = unpack("!Ih", DSP)[0]  # DECP means De-encrypted Packet
+		DECP = unpack("cc????????????????fffff", DSP)[0]  #DECP means De-encrypted Packet
 		print(DECP)
 		
-#frc_2015.decryptPacket()		
+#decrypt.decryptPacket()		
